8964b2f29134ecb8c587d69da08367ab156c321a
Czkafek <czkafek@gmail.com>
2025-08-25 21:29:54 +0200

    plakatowanie i ciasta

---
+++ ciasta/ciasta.cpp
@@ -0,0 +1,38 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+const int MAXN = 1e6 + 10;
+const int MOD = 1e9 + 33;
+bool isUsed[MAXN];
+vector<int> prime;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	// Sito
+	for (int i = 2; i < n + 1; i++) {
+		if (!isUsed[i]) {
+			prime.push_back(i);
+			for (int j = i + i; j < n + 1; j += i) {
+				isUsed[j] = true;
+			}
+		}
+	}
+
+	ll wynik = 1;
+
+	for (int i = 0; i < prime.size(); i++) {
+		int count = 0;
+		ll val = prime[i];
+		while (val <= n) {
+			count++;
+			val *= prime[i];
+		}
+		wynik = (wynik * count) % MOD;
+	}
+
+	cout << wynik;
+}
---
+++ plakatowanie/plakatowanie.cpp
@@ -0,0 +1,34 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+const int MAXN = 250000 + 10;
+ll buildings[MAXN];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+	for (int i = 1; i < n + 1; i++) {
+		int tmp;
+		cin >> tmp >> buildings[i];
+	}
+
+	vector<ll> posters;
+	int result = 1;
+	posters.push_back(buildings[1]);
+
+	for (int i = 1; i < n + 1; i++) {
+		while (!posters.empty() && buildings[i] < posters.back())
+			posters.pop_back();
+		if (posters.empty() || buildings[i] > posters.back()) {
+			posters.push_back(buildings[i]);
+			result++;
+		}
+	}
+
+	cout << result;
+}
