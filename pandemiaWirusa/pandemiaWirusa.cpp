0fb0aba7916583214680e60ebc73aba9cb9ba253
Czkafek <czkafek@gmail.com>
2025-08-28 17:18:39 +0200

    exercises done

---
+++ pandemiaWirusa/pandemiaWirusa.cpp
@@ -0,0 +1,53 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXK = 50 + 10;
+string models[MAXK];
+
+void solveModel(int index) {
+	string model = 'S' + models[index];
+	int l = 0, r = 0;
+	int healthy = 0, L_infected = 0, P_infected = 0;
+	while (++r < model.length()) {
+		if (model[r] == 'P') {
+			P_infected++;
+			if (model[l] == 'P')
+				P_infected += r - l - 1;
+			else
+				healthy += r - l - 1;
+			l = r;
+		}
+		else if (model[r] == 'L') {
+			L_infected++;
+			if (model[l] == 'P') {
+				L_infected += (r - l - 1) / 2;
+				P_infected += (r - l - 1) / 2;
+				healthy += (r - l - 1) % 2;
+			}
+			else
+				L_infected += r - l - 1;
+			l = r;
+		}
+	}
+	if (model[l] == 'P')
+		P_infected += r - l - 1;
+	else
+		healthy += r - l - 1;
+
+	cout << L_infected << " " << P_infected << " " << healthy << "\n";
+}
+
+
+int main()
+{
+	int k;
+	cin >> k;
+
+	for (int i = 0; i < k; i++) {
+		int a;
+		cin >> a >> models[i];
+	}
+
+	for (int i = 0; i < k; i++)
+		solveModel(i);
+}
---
+++ quickSort/quickSort.cpp
@@ -0,0 +1,52 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 1e6 + 10;
+int numbers[MAXN];
+int counter = 0;
+
+int podziel(int l, int r) {
+	long long mid = numbers[(l + r) / 2];
+	int i = l - 1;
+	int j = r + 1;
+
+	while (true) {
+		while (numbers[++i] < mid);
+		while (numbers[--j] > mid);
+		if (i >= j)
+			break;
+		swap(numbers[i], numbers[j]);
+	}
+	
+	return j;
+}
+void quickSort(int l, int r, int n) {
+	if (l >= r) return;
+
+	int p = podziel(l, r);
+	quickSort(l, p, n);
+	quickSort(p+1, r, n);
+	cout << "\n#" << ++counter << "\n";
+	for (int i = 0; i < n; i++)
+		cout << numbers[i] << " ";
+}
+
+
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+		cin >> numbers[i];
+
+	quickSort(0, n-1, n);
+
+
+	cout << "\n\n\n";
+
+	for (int i = 0; i < n; i++)
+		cout << numbers[i] << " ";
+
+}
