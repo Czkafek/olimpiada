44de74ce2f4a9ae6ba1be69881443194ec66c080
Czkafek <czkafek@gmail.com>
2025-09-03 17:58:46 +0200

    ZADANIE Z IOI JEEEST

---
+++ poi/poi.cpp
@@ -0,0 +1,66 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 2000 + 10;
+int results[MAXN][MAXN];
+int points[MAXN];
+tuple<int, int, int> playersScore[MAXN];
+
+int main()
+{
+
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+	int n, t, p;
+	cin >> n >> t >> p;
+
+	for (int i = 1; i < n + 1; i++)
+		for (int j = 1; j < t + 1; j++)
+			cin >> results[i][j];
+
+	for (int i = 1; i < t + 1; i++) {
+		points[i] = 0;
+		for (int j = 1; j < n + 1; j++)
+			points[i] += results[j][i];
+		points[i] = n - points[i];
+	}
+
+	for (int i = 1; i < n + 1; i++) {
+		int score = 0;
+		int amount = 0;
+		for (int j = 1; j < t + 1; j++) {
+			if (results[i][j] == 1) {
+				score += points[j];
+				amount++;
+			}
+		}
+		playersScore[i] = make_tuple(score, amount, i);
+	}
+
+	sort(playersScore + 1, playersScore + n + 1, [](tuple<int, int, int> a, tuple<int, int, int> b) {
+			if (get<0>(a) == get<0>(b)) {
+				if (get<1>(a) == get<1>(b))
+					return get<2>(a) < get<2>(b);
+				return get<1>(a) > get<1>(b);
+			}
+			return get<0>(a) > get<0>(b);
+
+		});
+
+	for (int i = 1; i < n + 1; i++) {
+		if (get<2>(playersScore[i]) == p) {
+			cout << get<0>(playersScore[i]) << " " << i;
+			break;
+		}
+	}
+
+}
+
+/*
+for (int i = 1; i < n + 1; i++) {
+	cout << '\n';
+	cout << i << ". ID: " << get<2>(playersScore[i]) << " AMOUNT: " << get<1>(playersScore[i]) << " RESULT: " << get<0>(playersScore[i]);
+	cout << '\n';
+}
+*/
---
+++ tablice2/tablice2.cpp
@@ -0,0 +1,87 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+// ALGORYTM:
+// 1. Mapuję pierwszą tablicę - dla każdej liczby jest przydzielona para<kolumna, wiersz>
+// 2. Zaczynam przechodzić się po wszystkich początakch kolumn i wierszy
+// 3. Dla każdego początku sprawdzam i zapisuje w tymczasowej zmiennej kolumne/wiersz,
+//    który jest przydzielony danej liczbie i sprawdzam czy następne liczby z danej
+//    kolumny/wiersza wskazują tą samą liczbę
+
+const int MAXN = 1000;
+const int MAXVAL = 2 * 1e6 + 10;
+const int CONST_ADDITION = 1e6;
+int tab2[MAXN][MAXN];
+pair<int, int> coordinates[MAXVAL];
+string results[10 + 2];
+
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+	int t, n, m;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+
+		bool isAble = true;
+
+		cin >> n >> m; // m - kolumny, n - wiersze
+
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < m; k++) {
+				int tmp;
+				cin >> tmp;
+				coordinates[tmp + CONST_ADDITION] = make_pair(k, j); // <kolumna, wiersz>
+			}
+		}
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < m; k++) {
+				cin >> tab2[k][j];
+			}
+		}
+
+		for (int j = 0; j < m; j++) {
+			int colIndex = coordinates[tab2[j][0] + CONST_ADDITION].first;
+			for (int k = 0; k < n; k++)
+				if (colIndex != coordinates[tab2[j][k] + CONST_ADDITION].first) {
+					isAble = false;
+					break;
+				}
+		}
+
+		for (int j = 0; j < n; j++) {
+			int colIndex = coordinates[tab2[0][j] + CONST_ADDITION].second;
+			for (int k = 0; k < m; k++)
+				if (colIndex != coordinates[tab2[k][j] + CONST_ADDITION].second) {
+					isAble = false;
+					break;
+				}
+		}
+
+		if (isAble)
+			results[i] = "TAK";
+		else
+			results[i] = "NIE";
+
+	}
+
+	for (int i = 0; i < t; i++) {
+		cout << results[i] << '\n';
+	}
+}
+
+// ALGORYTM 2:
+// Wczytuje obydwie tablice, a następnie tworzy z każdego wiersza i kolumny danej tablicy tablice z vectorami
+// Na jedną tablice przypadają dwie tablice z maksymalnie 1000 vectorów i każdy vector po maks 1000 int'ów
+// Każdy taki vector jest posortowany
+// Po dodaniu wszystkich vectorów do tablicy, sortujemy tablice, i następnie porównujemy tablice
+// Jeśli tablice są takie same -> wypisujemy "TAK", jeśli nie -> "NIE"
+// Funkcja std::sort() ma złożoność czasową O(nlogn), sprawdźmy złożoność czasową sprawdzenia jednej pary tablic dla n = 1000 i m = 1000
+// Funkcja sort wykona się dla każdego vectora, vectorów będzie 1000 na jedną tablicę, więc wykona się 4000 sortowań
+// Jeden vector ma 1000 elementów, więc posortowanie jednego vectora zajmie około 1000 * 10 = 1e4 operacji
+// 4 * 1e3 * 1e4 = 4 * 1e7 -> Mniej więcej złożoność czasowa sprawdzenia jednej pary
+// Jeśli będziemy mieli 10 takich par to złożoność czasowa będzie wynosiła:
+// 4 * 1e7 * 10 = 4 * 1e8 -> Nie dodaliśmy do tego jeszcze operacji tworzenia tych vectorów, operacji wczytywania i wszystkich innych operacji
+// To rozwiązanie nie zmieści się w limicie czasowym
